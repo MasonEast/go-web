c0c89565eda10c0afa1900d6380e58e0ed79f8af
mason <jianfeng.liu@amh-group.com>
2022-04-26 14:32:26 +0800

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/user/:name/*action", func(c *gin.Context){
+		name := c.Param("name")
+		action := c.Param("action")
+		c.String(http.StatusOK, name, action)
+	})
+
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
